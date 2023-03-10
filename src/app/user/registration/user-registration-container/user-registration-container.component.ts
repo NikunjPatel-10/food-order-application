76e44dbb9b2a8381ee62651f1cded8dc2d5da3d1
NikunjPatel-10 <nikunj199842@gmail.com>
2023-03-10 11:26:06 +0530

    feat:registration

---
+++ src/app/user/registration/user-registration-container/user-registration-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registration-container',
+  templateUrl: './user-registration-container.component.html',
+  styleUrls: ['./user-registration-container.component.scss']
+})
+export class UserRegistrationContainerComponent {
+
+}
---
+++ src/app/user/registration/user-registration-container/user-registration-presentation/user-registration-presentation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registration-presentation',
+  templateUrl: './user-registration-presentation.component.html',
+  styleUrls: ['./user-registration-presentation.component.scss']
+})
+export class UserRegistrationPresentationComponent {
+
+}
