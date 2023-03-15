853723d7ebe52ef9ee340c45c43c411ac4213cdf
NikunjPatel-10 <nikunj199842@gmail.com>
2023-03-15 19:34:52 +0530

    feat:food-item page

--- src/app/food/food-item-container/food-item-presentation/food-item-presentation.component.ts
+++ src/app/food/food-item-container/food-item-presentation/food-item-presentation.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FoodItemPresenterService } from '../food-item-presenter/food-item-presenter.service';
 
 @Component({
   selector: 'app-food-item-presentation',
   templateUrl: './food-item-presentation.component.html',
-  styleUrls: ['./food-item-presentation.component.scss']
+  styleUrls: ['./food-item-presentation.component.scss'],
+  viewProviders: [FoodItemPresenterService]
 })
 export class FoodItemPresentationComponent implements OnInit {
   @Input() set foodDetail(res: any | null) {
@@ -18,11 +20,26 @@ export class FoodItemPresentationComponent implements OnInit {
     return this._foodDetail
   }
   private _foodDetail: any
-  constructor() {
 
+  // public pizzaPrice: any
+  // public price = 125;
+  // public isActive: boolean;
+  // public small?: boolean;
+  // public medium?: boolean;
+  // public large?: boolean;
+  constructor(private _foodItemPresenterService: FoodItemPresenterService) {
+    // this.isActive = true
   }
 
   ngOnInit(): void {
-
+    // this._foodItemPresenterService.pizzaPrice$.subscribe(res => {
+    //   this.pizzaPrice = res
+      
+    // })
+    // this.showPrice(1)
   }
+
+  // public showPrice(price: number) {  
+  //   this._foodItemPresenterService.showPrice(price,this.small!,this.medium!,this.large!)
+  // }
 }
--- src/app/food/food-item-container/food-item-presenter/food-item-presenter.service.ts
+++ src/app/food/food-item-container/food-item-presenter/food-item-presenter.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class FooddetailPresenterService {
+@Injectable()
+export class FoodItemPresenterService {
+  // public data: any[];
+  // public priceList: any;
+  // public pizzaPrice: Subject<any>;
+  // public pizzaPrice$: Observable<any>
+  constructor() {
+    //   this.pizzaPrice = new Subject(),
+    //  this.pizzaPrice$ = this.pizzaPrice.asObservable()
+    //   this.data = [125, 250, 400]
+  }
 
-  constructor() { }
+
+  // public showPrice(price: number,small:boolean,medium:boolean,large:boolean) {
+
+  //   if (price === 1) {
+  //     this.priceList = this.data[0]
+  //   }
+  //   else if (price === 2) {
+  //     this.priceList = this.data[1]
+  //   }
+
+  //   else if (price === 3) {
+  //     this.priceList = this.data[2]
+  //   }
+  //   this.pizzaPrice.next(this.priceList)
+
+  // }
 }
+
